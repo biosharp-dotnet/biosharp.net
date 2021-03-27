6d4421331edbec0a2d7036e2cd99b22421587cfd
Abhinav Sharma <abhi18av@gmail.com>
2021-03-27 16:58:49 -0300

    Update the home page and the initial blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
+import BioSharpLogoImage from "@site/static/img/biosharp_logo.png";
 
 export default function Home() {
   const context = useDocusaurusContext();
@@ -17,47 +18,59 @@ export default function Home() {
       <main>
         <div class="hero shadow--lw">
           <div class="container">
-            <a href="https://company.e-resident.gov.ee/company/16019987">
-              <h1 class="hero__title">BioSharp OÜ</h1>
-            </a>
-            <p class="hero__subtitle">
-              We make Bioinformatics simple and easy.
-            </p>
-            <div>
-              <a
-                class="button button--outline button--secondary"
-                href="https://github.com/biosharp-dotnet"
-              >
-                Github
-              </a>
+            <div class="row row--no-gutters">
+              <div class="col "></div>
+              <div class="col col--8">
+                {/* <h1 class="hero__title">BioSharp OÜ</h1> */}
+                <img src={BioSharpLogoImage} width={234} height={45} />
+                <p class="hero__subtitle">
+                  We make Bioinformatics simple and easy.
+                </p>
+                <div>
+                  <a
+                    class="button button--outline button--secondary"
+                    href="https://company.e-resident.gov.ee/company/16019987"
+                  >
+                    Estonian Marketplace
+                  </a>
 
-              <a
-                class="button button--outline button--secondary"
-                href="https://www.linkedin.com/company/biosharp"
-              >
-                LinkedIn
-              </a>
+                  <a
+                    class="button button--outline button--secondary"
+                    href="https://github.com/biosharp-dotnet"
+                  >
+                    Github
+                  </a>
 
-              <a
-                class="button button--outline button--secondary"
-                href="https://www.youtube.com/channel/UChZX3OqkjS1SuGV_XPLPZ-Q?view_as=subscriber"
-              >
-                YouTube
-              </a>
+                  <a
+                    class="button button--outline button--secondary"
+                    href="https://www.linkedin.com/company/biosharp"
+                  >
+                    LinkedIn
+                  </a>
 
-              <a
-                class="button button--outline button--secondary"
-                href="https://www.facebook.com/BioSharp.net/"
-              >
-                Facebook
-              </a>
+                  <a
+                    class="button button--outline button--secondary"
+                    href="https://www.youtube.com/channel/UChZX3OqkjS1SuGV_XPLPZ-Q?view_as=subscriber"
+                  >
+                    YouTube
+                  </a>
 
-              <a
-                class="button button--outline button--secondary"
-                href="https://twitter.com/BioSharp_dotnet"
-              >
-                Twitter
-              </a>
+                  <a
+                    class="button button--outline button--secondary"
+                    href="https://www.facebook.com/BioSharp.net/"
+                  >
+                    Facebook
+                  </a>
+
+                  <a
+                    class="button button--outline button--secondary"
+                    href="https://twitter.com/BioSharp_dotnet"
+                  >
+                    Twitter
+                  </a>
+                </div>
+              </div>
+              <div class="col " />
             </div>
           </div>
         </div>
