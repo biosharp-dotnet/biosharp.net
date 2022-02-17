519fdff7b2dd370165d748b58eaed7b067e15923
Abhinav Sharma <abhi18av@outlook.com>
2022-02-17 09:09:59 +0000

    Add social media icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
 import BioSharpLogoImage from "@site/static/img/biosharp_logo.png";
+import { SocialIcon } from 'react-social-icons';
 
 export default function Home() {
   const context = useDocusaurusContext();
@@ -19,59 +20,54 @@ export default function Home() {
         <div class="hero shadow--lw">
           <div class="container">
             <div class="row row--no-gutters">
-              <div class="col "></div>
-              <div class="col col--8">
+              <div class="col"> </div>
+
+              <div class="col col--offset-2">
                 {/* <h1 class="hero__title">BioSharp OÜ</h1> */}
                 <img src={BioSharpLogoImage} width={234} height={45} />
                 <p class="hero__subtitle">
                   We make Bioinformatics simple and easy.
                 </p>
+
+
+
                 <div>
-                  <a
-                    class="button button--outline button--secondary"
-                    href="https://company.e-resident.gov.ee/company/16019987"
-                  >
-                    Estonian Marketplace
-                  </a>
 
-                  <a
-                    class="button button--outline button--secondary"
-                    href="https://github.com/biosharp-dotnet"
-                  >
-                    Github
-                  </a>
+                  <SocialIcon url="https://github.com/biosharp-dotnet" />
+                  <SocialIcon url="https://www.linkedin.com/company/biosharp" />
+                  <SocialIcon url="https://www.youtube.com/channel/UChZX3OqkjS1SuGV_XPLPZ-Q?view_as=subscriber" />
+                  <SocialIcon url="https://www.facebook.com/BioSharp.net/" />
+                  <SocialIcon url="https://twitter.com/BioSharp_dotnet" />
 
-                  <a
-                    class="button button--outline button--secondary"
-                    href="https://www.linkedin.com/company/biosharp"
-                  >
-                    LinkedIn
-                  </a>
+                </div>
 
-                  <a
-                    class="button button--outline button--secondary"
-                    href="https://www.youtube.com/channel/UChZX3OqkjS1SuGV_XPLPZ-Q?view_as=subscriber"
-                  >
-                    YouTube
-                  </a>
-
-                  <a
-                    class="button button--outline button--secondary"
-                    href="https://www.facebook.com/BioSharp.net/"
-                  >
-                    Facebook
-                  </a>
-
-                  <a
-                    class="button button--outline button--secondary"
-                    href="https://twitter.com/BioSharp_dotnet"
-                  >
-                    Twitter
-                  </a>
+              </div>
+{/* 
+              <div class="card padding--lg padding-left--lg margin-left--lg">
+                <div class="card__body">
+                  <h4>Estonian Marketplace profile</h4>
+                  <small>
+                    BioSharp OÜ is a company registered in Estonia, Europe.
+                  </small>
+                </div>
+                <div class="card__footer">
+                  <button class="button ">
+                    <a href="https://company.e-resident.gov.ee/company/16019987" > Visit company page </a>
+                  </button>
                 </div>
               </div>
-              <div class="col " />
+ */}
+
+
+              <div class="col"> </div>
+
+
             </div>
+
+            {/* <button class="button button--outline button--primary">
+              <a href="https://company.e-resident.gov.ee/company/16019987" > Estonian Marketplace </a>
+            </button> */}
+
           </div>
         </div>
       </main>
